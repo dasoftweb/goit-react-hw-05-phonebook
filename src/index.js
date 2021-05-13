25e9f0746ba2d1d2239161a43853fa72bc248453
Klirik <dasoftweb@gmail.com>
2021-05-13 19:37:03 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import 'modern-normalize/modern-normalize.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles/style.scss';
+
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ src/redux/contacts/helpers.js
@@ -0,0 +1,14 @@
+// const capitalizeFirstLetter = string => {
+//   console.log('capitalizeFirstLetter');
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// };
+
+// const setContactId = contacts => {
+//   console.log(contacts);
+//   const contactIds = [];
+//   contacts.map(contact => contactIds.push(contact.id.replace(/\D/g, '')));
+//   console.log('setContactId');
+//   return 'id-' + (Math.max(...contactIds) + 1);
+// };
+
+// export default { capitalizeFirstLetter, setContactId };
